13b6879a4fc47ad22c417606ebc05ef1f22342db
akshit36 <146572624+akshit36@users.noreply.github.com>
2023-12-10 19:55:49 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    char s[1000];
+    int i, words = 0;
+
+    printf("Enter the string: ");
+    scanf("%[^\n]s", s);
+
+    for (i = 0; s[i]; i++)
+    {
+        if (s[i] == ' ')
+            words++;
+    }
+
+    if (i > 0)
+        words++;
+
+    printf("Number of words in the string: %d\n", words);
+
+    return 0;
+}
